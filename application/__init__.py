63c8e5dba71538d592654d47bb3eaf97189992f7
brandon-wallace <17948072+brandon-wallace@users.noreply.github.com>
2020-11-22 16:40:13 -0500

    Add Flask Migrate

--- application/__init__.py
+++ application/__init__.py
@@ -3,12 +3,12 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_migrate import Migrate
 # from flask_debugtoolbar import DebugToolbarExtension
-import requests
-from bs4 import BeautifulSoup
 
 
 db = SQLAlchemy()
+migrate = Migrate()
 bcrypt = Bcrypt()
 login_manager = LoginManager()
 login_manager.login_view = 'auth.login'
@@ -26,6 +26,7 @@ def create_app():
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
+    migrate.init_app(app, db)
     bcrypt.init_app(app)
     login_manager.init_app(app)
     # toolbar.init_app(app)
@@ -37,18 +38,3 @@ def create_app():
     app.register_blueprint(auth)
 
     return app
-
-
-def scraper():
-    '''Scrap word of the day'''
-
-    response = requests.get("https://www.spanishdict.com/wordoftheday")
-    html = BeautifulSoup(response.content, 'html.parser')
-    sentences = html.find_all("div", attrs={'class': 'sd-wotd-example-source'})
-    data = {}
-
-    for sentence in sentences:
-        s = sentence.string
-        data[s] = sentence.attrs['class']
-        print(data[s])
-    return
