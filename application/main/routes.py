0d8fd03e8fc1804e4850c338b071dccae304f36e
brandon-wallace <17948072+brandon-wallace@users.noreply.github.com>
2020-11-27 05:28:40 -0500

    Set query limit

--- application/main/routes.py
+++ application/main/routes.py
@@ -20,7 +20,7 @@ def index():
     # page = request.args.get(get_page_parameter(), 1, type=int)
     # page = request.args.get('page', 1, type=int)
     # words = Word.query.order_by(func.random()).paginate(page, per_page=5)
-    words = Word.query.order_by(func.random()).limit(2)
+    words = Word.query.order_by(func.random()).limit(1)
     # words = Word.query.paginate(page=page, per_page=2)
     # words = Word.query.order_by(func.random())
     # pagination = Pagination(page=page, total=words.count(), search=False,
