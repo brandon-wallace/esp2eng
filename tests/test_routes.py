84c9dcf5ec06b689af17accad65d51d707df8d28
brandon-wallace <17948072+brandon-wallace@users.noreply.github.com>
2021-05-07 13:14:54 -0400

    config: Add Pytest
    Pytest was added to project to test the code.

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -11,3 +11,4 @@ def test_index():
     with app.test_client() as test_client:
         response = test_client.get('/')
         assert response.status_code == 200
+        assert b'The Place to Learn Spanish' in response.data
